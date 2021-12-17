5682bad3638b7e6051147e47f54f3f4e6b1880e7
Farzan-ul-haq <farzanulhaq123@gmail.com>
2021-12-17 19:21:18 +0500

    fix: launch error

--- DoorAutomation/settings.py
+++ DoorAutomation/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +88,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
