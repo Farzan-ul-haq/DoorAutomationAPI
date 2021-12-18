7086bebd8fdfc1fb1194c13f517e1ffd34a97f80
Farzan Ul Haq Siddiqui <57554503+Farzan-ul-haq@users.noreply.github.com>
2021-12-18 18:20:59 +0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
 from . import views
+
+
 urlpatterns = [
     path('door/<int:id>', views.DoorDetailView.as_view()),
+    path('door/<int:id>/password/', views.DoorPassword.as_view()),
+    path('door/<int:id>/open/',views.OpenDoor.as_view()),
+    path('door/<int:id>/close/',views.CloseDoor.as_view()),
 ]
